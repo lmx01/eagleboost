909de3d1164b777d885000a0037d4e09bac3ce04
eagleboost <eagleboost@msn.com>
2019-03-23 22:57:05 -0500

    Add XmlnsDefinition and XmlnsPrefix

--- eagleboost.shell/Properties/AssemblyInfo.cs
+++ eagleboost.shell/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -20,6 +21,9 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4515AA2F-B031-4018-AFE9-29FE3E34255F")]
+
+[assembly: XmlnsDefinition("http://www.eagleboost.com/xaml/presentation", "eagleboost.shell.Controls")]
+[assembly: XmlnsPrefix("http://www.eagleboost.com/xaml/presentation", "ebsh")]
 
 // Version information for an assembly consists of the following four values:
 //
