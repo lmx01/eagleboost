480326bd3e7e390a83b4d0681d82e15fe156b8fe
eagleboost <eagleboost@msn.com>
2019-07-22 08:17:39 -0500

    Call ExpandAsync instead of LoadChildrenAsync to make sure the node is sexpanded

--- eagleboost.shell/FileSystems/ViewModels/FileSystemTreeViewModel.cs
+++ eagleboost.shell/FileSystems/ViewModels/FileSystemTreeViewModel.cs
@@ -110,7 +110,7 @@ namespace eagleboost.shell.FileSystems.ViewModels
 
       while (folderStack.Count > 0)
       {
-        await node.LoadChildrenAsync().ConfigureAwait(true);
+        await node.ExpandAsync().ConfigureAwait(true);
 
         var folder = folderStack.Pop();
         var childNode = node.Children.FirstOrDefault(i => folder == i.DataItem.CastTo<IFile>().Name);
