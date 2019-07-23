cc562da2aac8f5434b8c76c272c2b9ecbf87075e
eagleboost <eagleboost@msn.com>
2019-07-22 23:39:48 -0500

    Add GoogleDrivePopupTreeControl

---
+++ eagleboost.googledrive/Views/GoogleDrivePopupTreeControl.cs
@@ -0,0 +1,59 @@
+﻿// // Author   : Shuo Zhang
+// // E-MAIL   : eagleboost@msn.com
+// // Creation : 22 9:50 PM
+
+namespace eagleboost.googledrive.Views
+{
+  using System.ComponentModel;
+  using System.Windows;
+  using System.Windows.Data;
+  using System.Windows.Threading;
+  using eagleboost.googledrive.ViewModels;
+  using eagleboost.presentation.Extensions;
+  using eagleboost.UserExperience.Controls;
+
+  /// <summary>
+  /// GoogleDrivePopupTreeControl
+  /// </summary>
+  public class GoogleDrivePopupTreeControl : StaThreadContainer
+  {
+    #region Overrides
+    protected override FrameworkElement CreateControl(object initParams)
+    {
+      var param = (GoogleDrivePopupTreeParams) initParams;
+      var vm = new GoogleDriveTreeViewModel {UpdateFrequentFolder = true};
+      vm.Initialize(param.DriveService);
+      if (param.FrequentFiles != null)
+      {
+        vm.SetFrequentFiles(param.FrequentFiles);
+      }
+      vm.PropertyChanged += HandleViewModelPropertyChanged;
+      var tree = new GoogleDriveTreeView {DataContext = vm};
+      tree.SetBinding(GoogleDriveTreeView.ItemsSourceProperty, new Binding("ItemsView"));
+      tree.SetBinding(GoogleDriveTreeView.SelectedItemProperty,
+        new Binding("SelectedItem") {Mode = BindingMode.TwoWay});
+
+      tree.Dispatcher.BeginInvoke(async () =>
+      {
+        await vm.InitializeAsync(false).ConfigureAwait(true);
+        var path = param.Path;
+        await vm.SelectAsync(path.Split(',')).ConfigureAwait(true);
+      }, DispatcherPriority.Background);
+
+      return tree;
+    }
+
+    #endregion Overrides
+
+    #region Event Handlers
+    private void HandleViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+      if (e.PropertyName == "SelectedItem")
+      {
+        var vm = (GoogleDriveTreeViewModel) sender;
+        UpdateState(vm.SelectedItem);
+      }
+    }
+    #endregion Event Handlers
+  }
+}
---
+++ eagleboost.googledrive/Views/GoogleDrivePopupTreeParams.cs
@@ -0,0 +1,21 @@
+﻿// // Author   : Shuo Zhang
+// // E-MAIL   : eagleboost@msn.com
+// // Creation : 22 9:53 PM
+
+namespace eagleboost.googledrive.Views
+{
+  using eagleboost.googledrive.Contracts;
+  using eagleboost.shell.FileSystems.Models;
+
+  /// <summary>
+  /// GoogleDrivePopupTreeParams
+  /// </summary>
+  public class GoogleDrivePopupTreeParams
+  {
+    public IGoogleDriveService DriveService { get; set; }
+
+    public string Path { get; set; }
+
+    public FileFrequency[] FrequentFiles { get; set; }
+  }
+}
