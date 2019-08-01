f004f331ccec4f0b53830903c61633443409420d
eagleboost <eagleboost@msn.com>
2019-07-31 22:05:45 -0500

    Add MessageBoxActivity

---
+++ eagleboost.interaction/Common/MessageBoxActivity.cs
@@ -0,0 +1,26 @@
+﻿// // Author   : Shuo Zhang
+// // E-MAIL   : eagleboost@msn.com
+// // Creation : 31 9:49 PM
+
+namespace eagleboost.interaction.Common
+{
+  using System.Windows;
+  using eagleboost.interaction.Activities;
+  using eagleboost.presentation.Controls.MessageBoxes;
+
+  /// <summary>
+  /// MessageBoxActivity
+  /// </summary>
+  public class MessageBoxActivity : DialogActivity<AsyncActivityResult<MessageBoxViewModel>>
+  {
+    #region Overrides
+    protected override void ShowDialog()
+    {
+      var viewModel = ActivityArgs.GetArgs<MessageBoxViewModel>() ?? new MessageBoxViewModel();
+      viewModel.Result = MessageBox.Show(viewModel.Text, viewModel.Header, viewModel.MessageBoxButton, viewModel.MessageBoxImage);
+      var result = new AsyncActivityResult<MessageBoxViewModel>(true, viewModel);
+      Completion.TrySetResult(result);
+    }
+    #endregion Overrides
+  }
+}
---
+++ eagleboost.presentation/Controls/MessageBoxes/MessageBoxViewModel.cs
@@ -0,0 +1,27 @@
+﻿// // Author   : Shuo Zhang
+// // E-MAIL   : eagleboost@msn.com
+// // Creation : 31 9:49 PM
+
+namespace eagleboost.presentation.Controls.MessageBoxes
+{
+  using System.Windows;
+  using eagleboost.presentation.Contracts;
+
+  /// <summary>
+  /// MessageBoxViewModel
+  /// </summary>
+  public class MessageBoxViewModel : IHeader
+  {
+    #region Public Properties
+    public string Header { get; set; }
+
+    public string Text { get; set; }
+
+    public MessageBoxButton MessageBoxButton { get; set; }
+
+    public MessageBoxImage MessageBoxImage { get; set; }
+
+    public MessageBoxResult Result { get; set; }
+    #endregion Public Properties
+  }
+}
